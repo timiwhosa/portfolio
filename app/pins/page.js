37d4fa05cb366af4dc0c14c83e810b7378f0db91
timiwhosa <timiwhosa@gmail.com>
2024-08-22 08:15:51 +0100

    apin loading animation

--- app/pins/page.js
+++ app/pins/page.js
@@ -1,6 +1,8 @@
 "use client";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
 export default function Pins() {
   return (
     <div className="pb-10">
@@ -41,14 +43,50 @@ export default function Pins() {
 }
 
 export function Apin({ img, title, pinterestLink }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  const handleImageError = () => {
+    setIsLoading(false);
+    // You could set an error state here if you want to handle loading errors
+  };
   return (
     <Link href={pinterestLink} legacyBehavior>
       <a target="_blank" rel="noopener noreferrer">
         <div className="grid gap-2">
+          {isLoading && (
+            <div className="loading-spinner flex h-full items-center">
+              <svg
+                className="animate-spin h-12 w-12 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v2a6 6 0 00-6 6H4z"
+                />
+              </svg>
+            </div>
+          )}
           <img
             src={img}
             width={200}
             height={200}
+            onLoad={handleImageLoad}
+            onError={handleImageError}
             className="w-full h-auto rounded-lg border-2 border-black"
             alt="pinterest pins"
           />
